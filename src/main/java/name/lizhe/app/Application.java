9381f8ffa0c2daea89d1d4a70dd3a76a64da291e
Lizhe <Lizhe@Lizhe-PC>
2018-02-06 17:04:08 +0800

    8081

---
+++ src/main/java/name/lizhe/app/Application.java
@@ -0,0 +1,13 @@
+package name.lizhe.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan(basePackages={"name.lizhe.module.*"}) 
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
