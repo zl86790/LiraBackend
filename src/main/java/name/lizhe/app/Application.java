4514f709b0b3945272e41b58dc4dd0d29341cb0c
Lizhe <LZ@LZ-PC>
2018-02-06 23:08:57 +0800

    added WebMvcConfigurer

--- src/main/java/name/lizhe/app/Application.java
+++ src/main/java/name/lizhe/app/Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages={"name.lizhe.module.*"}) 
+@ComponentScan(basePackages={"name.lizhe.*.*"}) 
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
