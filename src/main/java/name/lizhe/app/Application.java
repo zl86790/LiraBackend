146574d11a55f9a58933f72e96268557bd84054d
Lizhe <LZ@LZ-PC>
2018-02-07 21:48:36 +0800

    Revert "added WebMvcConfigurer"
    This reverts commit 4514f709b0b3945272e41b58dc4dd0d29341cb0c.

--- src/main/java/name/lizhe/app/Application.java
+++ src/main/java/name/lizhe/app/Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages={"name.lizhe.*.*"}) 
+@ComponentScan(basePackages={"name.lizhe.module.*"}) 
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
--- src/main/java/name/lizhe/module/login/controller/HomeController.java
+++ src/main/java/name/lizhe/module/login/controller/HomeController.java
@@ -1,28 +1,12 @@
 package name.lizhe.module.login.controller;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @RestController
 public class HomeController {
-//	@CrossOrigin(origins = "http://localhost:8080")
 	@RequestMapping(value="/login.do")
 	public String gotoHome(){
 		return "hello world";
 	}
-	
-	@Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurerAdapter() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedOrigins("http://localhost:8080");
-            }
-        };
-    }
 }
