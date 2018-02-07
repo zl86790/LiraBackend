6122022316fb77cd234267f9a124b87a654f05b1
Lizhe <LZ@LZ-PC>
2018-02-07 22:20:13 +0800

    token worked

---
+++ src/main/java/name/lizhe/lira/app/Application.java
@@ -0,0 +1,42 @@
+package name.lizhe.lira.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import name.lizhe.lira.filter.JwtAuthenticationFilter;
+
+
+@SpringBootApplication
+@ComponentScan(basePackages={"name.lizhe.*.*"}) 
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+    
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("*");
+            }
+        };
+    }
+    
+    @Bean
+    public FilterRegistrationBean jwtFilter() {
+        final FilterRegistrationBean registrationBean = new FilterRegistrationBean();
+        JwtAuthenticationFilter filter = new JwtAuthenticationFilter(
+                "/api/**");
+        registrationBean.setFilter(filter);
+        return registrationBean;
+    }
+    
+}
---
+++ src/main/java/name/lizhe/lira/module/login/controller/HomeController.java
@@ -0,0 +1,46 @@
+package name.lizhe.lira.module.login.controller;
+
+import static name.lizhe.lira.app.tool.JwtUtil.HEADER_STRING;
+import static name.lizhe.lira.app.tool.JwtUtil.TOKEN_PREFIX;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import name.lizhe.lira.app.tool.JwtUtil;
+import name.lizhe.lira.module.user.bean.UserBean;
+
+@RestController
+public class HomeController {
+	
+    @GetMapping("/api/hello")
+    public @ResponseBody Object hellWorld() {
+        return "Hello World! This is a protected api";
+    }
+
+    @PostMapping("/login")
+    public void login(HttpServletResponse response,
+                      @RequestBody final UserBean userBean) throws IOException {
+        if(validCredentials(userBean)) {
+            String jwt = JwtUtil.generateToken(userBean.getUsername());
+            response.addHeader(HEADER_STRING, TOKEN_PREFIX + " " + jwt);
+        }else
+            response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Wrong credentials");
+    }
+
+    private boolean validCredentials(UserBean userBean) {
+        return "admin".equals(userBean.getUsername())
+                && "admin".equals(userBean.getPassword());
+    }
+
+	
+}
