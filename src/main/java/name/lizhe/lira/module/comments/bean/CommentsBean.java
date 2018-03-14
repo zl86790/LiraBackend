1a5b1b3ac26a56eb50eacf03fe027e45126d470b
Lizhe <Lizhe@Lizhe-PC>
2018-03-14 18:31:34 +0800

    updates

---
+++ src/main/java/name/lizhe/lira/module/comments/bean/CommentsBean.java
@@ -0,0 +1,26 @@
+package name.lizhe.lira.module.comments.bean;
+
+public class CommentsBean {
+	private int id;
+	private int issue_id;
+	private String content;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getIssue_id() {
+		return issue_id;
+	}
+	public void setIssue_id(int issue_id) {
+		this.issue_id = issue_id;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	
+}
