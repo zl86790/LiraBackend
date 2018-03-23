880e09671722bc95f08755d145e7399747726a85
Lizhe <Lizhe@Lizhe-PC>
2018-03-23 09:47:29 +0800

    added lira-issue-history

---
+++ modules/lira-issue-history/src/main/java/name/lizhe/lira/module/history/controller/HistoryController.java
@@ -0,0 +1,28 @@
+package name.lizhe.lira.module.history.controller;
+
+import java.io.IOException;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import name.lizhe.lira.module.history.bean.IssueHistoryBean;
+import name.lizhe.lira.module.history.service.IssueHistoryService;
+
+@RestController
+public class HistoryController {
+
+	@Autowired
+	IssueHistoryService issueHistoryService;
+	
+
+    @GetMapping("/api/v1/postlogin/issuehistories")
+    public @ResponseBody List<IssueHistoryBean> getIssueComments(String issue_id) throws IOException {
+    	
+    	return issueHistoryService.getIssueHistories(issue_id);
+    	
+    }
+
+}
---
+++ modules/lira-issue-history/src/main/java/name/lizhe/lira/module/history/dao/mapper/HistoryMapper.java
@@ -0,0 +1,12 @@
+package name.lizhe.lira.module.history.dao.mapper;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import name.lizhe.lira.module.history.bean.IssueHistoryBean;
+
+@Mapper
+public interface HistoryMapper {
+	public List<IssueHistoryBean> getIssueHistories(String issue_id);
+}
