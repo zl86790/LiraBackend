29a3b1d47dfdc450dad797be3fecb1aea2fad1b0
Lizhe <Lizhe@Lizhe-PC>
2018-03-23 17:42:48 +0800

    updates

--- modules/lira-system-parameter/src/main/java/name/lizhe/lira/module/sysparameter/controller/SysParameterController.java
+++ modules/lira-system-parameter/src/main/java/name/lizhe/lira/module/sysparameter/controller/SysParameterController.java
@@ -6,6 +6,7 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,7 +20,7 @@ public class SysParameterController {
 	SysParameterService sysParameterService;
 
    @GetMapping("/api/v1/postlogin/sysparameters")
-   public @ResponseBody List<SysParameterBean> getProjects(@RequestBody final Map<String,String> map){
+   public @ResponseBody List<SysParameterBean> getProjects(@RequestParam final Map<String,String> map){
 	   
 	   String module_key = map.get("module_key");
 	   String value_key = map.get("value_key");
