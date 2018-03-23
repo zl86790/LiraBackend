32648f99af2e72a3cc34e3fcbaf7401bd93021ff
Lizhe <Lizhe@Lizhe-PC>
2018-03-23 09:48:51 +0800

    added lira-project

---
+++ modules/lira-project/src/main/java/name/lizhe/lira/module/project/service/impl/ProjectServiceImpl.java
@@ -0,0 +1,28 @@
+package name.lizhe.lira.module.project.service.impl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import name.lizhe.lira.module.project.bean.ProjectBean;
+import name.lizhe.lira.module.project.dao.mapper.ProjectMapper;
+import name.lizhe.lira.module.project.service.ProjectService;
+
+@Service
+public class ProjectServiceImpl implements ProjectService {
+
+	@Autowired
+	ProjectMapper projectMapper;
+	
+	@Override
+	public void createProject(ProjectBean projectBean) {
+		projectMapper.createProject(projectBean);
+	}
+
+	@Override
+	public List<ProjectBean> getProjects() {
+		return projectMapper.getProjects();
+	}
+
+}
