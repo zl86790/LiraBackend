681e53bacb625e13e00719818ff5a65e7c4c99ee
Lizhe <Lizhe@Lizhe-PC>
2018-03-23 09:44:51 +0800

    remove old java files

--- src/main/java/name/lizhe/lira/module/comments/service/CommentsService.java
+++
@@ -1,9 +0,0 @@
-package name.lizhe.lira.module.comments.service;
-
-import java.util.List;
-
-import name.lizhe.lira.module.comments.bean.CommentsBean;
-
-public interface CommentsService {
-	public List<CommentsBean> getIssueComments(String issue_id);
-}
--- src/main/java/name/lizhe/lira/module/login/service/LoginService.java
+++
@@ -1,7 +0,0 @@
-package name.lizhe.lira.module.login.service;
-
-import name.lizhe.lira.module.user.bean.UserBean;
-
-public interface LoginService {
-	UserBean doLogin(String username,String password);
-}
