c0eb7e63a1569a8cadb9ddbbcb4742017b88158b
Lizhe <Lizhe@Lizhe-PC>
2018-05-10 14:27:07 +0800

    updates

--- modules/lira-issue/src/main/java/name/lizhe/lira/module/issue/service/impl/IssueServiceImpl.java
+++ modules/lira-issue/src/main/java/name/lizhe/lira/module/issue/service/impl/IssueServiceImpl.java
@@ -60,6 +60,7 @@ public class IssueServiceImpl implements IssueService {
 		map.put("resolved_time", issueBean.getResolved_time());
 		map.put("created_time", issueBean.getCreated_time());
 		map.put("assignee", issueBean.getAssignee());
+		map.put("reporter", issueBean.getReporter());
 		return issueMapper.updateIssueDes(map);
 	}
 
